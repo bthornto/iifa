baa2467b70c2fedbc21cb9f3f9678f2f4e2eba9b
Mike Gee <geezyx@gmail.com>
2014-10-23 20:52:37 -0700

    DAMN YOU ASSOCIATIONS!!!!

--- db/migrate/defect_cycle_join_table.rb
+++ db/migrate/defect_cycle_join_table.rb
@@ -1,6 +1,6 @@
 class CreateDefectTypeCycleJoinTable < ActiveRecord::Migration
   def change
-    create_table :defect_types_cycles, id: false do |t|
+    create_table :cycles_defect_types, id: false do |t|
       t.integer :defect_type_id
       t.integer :cycle_id
     end
